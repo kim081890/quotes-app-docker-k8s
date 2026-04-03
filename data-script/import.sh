154c310dbed51f7ed0ac795c5ce42abe849e6a01
Kim Kutrovacz <kim081890@users.noreply.github.com>
2026-04-03 18:03:50 +0000

    feat: working Kubernetes deployment with PVC, Secrets, and NodePort services

--- data-script/import.sh
+++ data-script/import.sh
@@ -1,1 +1,1 @@
-mysql -h $MYSQL_DB_HOST -u $MYSQL_USER -p${MYSQL_USER_PASSWORD} mydatabase < /opt/mydatabase.sql
+mysql --ssl=0 -h $MYSQL_DB_HOST -u $MYSQL_USER -p${MYSQL_USER_PASSWORD} mydatabase < /opt/mydatabase.sql
